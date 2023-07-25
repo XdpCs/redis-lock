2c0237279e4b068c83cb126ba9fa622d2f694d67
Alan Xu <xdpcsyy@gmail.com>
2023-07-25 23:35:28 +0800

    test(client): add client test and fix some bug
    add client test, fix bug because of context variable name and rename function name

--- client_test.go
+++ client_test.go
@@ -21,23 +21,29 @@ func TestNewClient(t *testing.T) {
 	// init actualOne
 	actualOne, err := NewClient(rdb)
 	if err != nil {
-		t.Fatal("actualOne NewClient error")
+		t.Fatalf("actualOne NewClient error:[%v]", err)
 	}
 
 	actualTwo, err := NewClient(rdb, WithCipherKey("11181114"))
 	if err != nil {
-		t.Fatal("actualTwo NewClient error")
+		t.Fatalf("actualTwo NewClient error:[%v]", err)
 	}
 
-	// init cipher
-	cipher, err := rc4.NewCipher([]byte("1114"))
+	// init cipherTwo
+	cipherTwo, err := rc4.NewCipher([]byte("11181114"))
 	if err != nil {
-		t.Fatal("init cipher error")
+		t.Fatalf("init cipherTwo error:[%v]", err)
 	}
 
-	actualThree, err := NewClient(rdb, WithCipher(cipher))
+	// init cipherThree
+	cipherThree, err := rc4.NewCipher([]byte("1114"))
 	if err != nil {
-		t.Fatal("actualThree NewClient error")
+		t.Fatalf("init cipherThree error:[%v]", err)
+	}
+
+	actualThree, err := NewClient(rdb, WithCipher(cipherThree))
+	if err != nil {
+		t.Fatalf("actualThree NewClient error:[%v]", err)
 	}
 
 	// test cases
@@ -53,12 +59,12 @@ func TestNewClient(t *testing.T) {
 		{
 			"NewClientWithCipherKey",
 			actualTwo,
-			&Client{redisClient: rdb, cipherKey: "11181114", Cipher: actualTwo.Cipher},
+			&Client{redisClient: rdb, cipherKey: "11181114", Cipher: cipherTwo},
 		},
 		{
 			Name:     "NewClientWithCipher",
 			Actual:   actualThree,
-			Expected: &Client{redisClient: rdb, cipherKey: "-1", Cipher: cipher},
+			Expected: &Client{redisClient: rdb, cipherKey: "-1", Cipher: cipherThree},
 		},
 	}
 
@@ -79,11 +85,17 @@ func TestNewDefaultClient(t *testing.T) {
 	// init redislock client
 	client, err := NewDefaultClient(rdb)
 	if err != nil {
-		t.Fatal("NewDefaultClient error")
+		t.Fatalf("NewDefaultClient error:[%v]", err)
+	}
+
+	cipherKey := "1118"
+	cipher, err := rc4.NewCipher([]byte(cipherKey))
+	if err != nil {
+		t.Fatalf("init cipher error:[%v]", err)
 	}
 
 	// test cases
-	compareClient(t, &Client{redisClient: rdb, cipherKey: "1118", Cipher: client.Cipher}, client)
+	compareClient(t, &Client{redisClient: rdb, cipherKey: cipherKey, Cipher: cipher}, client)
 }
 
 func TestClient_TryLock(t *testing.T) {
@@ -96,7 +108,7 @@ func TestClient_TryLock(t *testing.T) {
 	// init redislock client
 	client, err := NewDefaultClient(rdb)
 	if err != nil {
-		t.Fatal("NewDefaultClient error")
+		t.Fatalf("NewDefaultClient error:[%v]", err)
 	}
 	keyOne := "testOne"
 	keyTwo := "testTwo"
@@ -107,12 +119,12 @@ func TestClient_TryLock(t *testing.T) {
 
 	actualOne, err := client.TryLock(ctxOne, keyOne, -1)
 	if err != nil {
-		t.Fatal("actualOne TryLock error")
+		t.Fatalf("actualOne TryLock error:[%v]", err)
 	}
 
 	actualTwo, err := client.TryLock(ctxTwo, keyTwo, 10*time.Second)
 	if err != nil {
-		t.Fatal("actualTwo TryLock error")
+		t.Fatalf("actualTwo TryLock error:[%v]", err)
 	}
 
 	// test cases
@@ -154,6 +166,73 @@ func TestClient_TryLock(t *testing.T) {
 	}
 }
 
+func TestClient_TryLockWithRetryStrategy(t *testing.T) {
+	// init redis client
+	rdb := redis.NewClient(&redis.Options{
+		Addr: ":6379",
+	})
+	// close redis client
+	defer rdb.Close()
+	// init redislock client
+	client, err := NewDefaultClient(rdb)
+	if err != nil {
+		t.Fatalf("NewDefaultClient error:[%v]", err)
+	}
+	keyOne := "testOne"
+	keyTwo := "testTwo"
+	defer teardown(t, rdb, []string{keyOne, keyTwo})
+
+	ctx := context.Background()
+
+	actualOne, err := client.TryLockWithRetryStrategy(ctx, keyOne, -1, NewNoRetry())
+	if err != nil {
+		t.Fatalf("actualOne TryLockWithRetryStrategy error:[%v]", err)
+	}
+
+	actualTwo, err := client.TryLockWithRetryStrategy(ctx, keyTwo, 30*time.Second, NewAverageRetry(3, 10*time.Second))
+	if err != nil {
+		t.Fatalf("actualTwo TryLockWithRetryStrategy error:[%v]", err)
+	}
+
+	// test cases
+	cases := []struct {
+		Name     string
+		Actual   *Mutex
+		Expected *Mutex
+	}{
+		{
+			"TryLockWithRetryStrategy",
+			actualOne,
+			&Mutex{
+				client:        client,
+				key:           keyOne,
+				expiration:    30 * time.Second,
+				value:         actualOne.value,
+				retryStrategy: NewNoRetry(),
+				watchDog:      actualOne.watchDog,
+			},
+		},
+		{
+			"TryLockWithExpiration",
+			actualTwo,
+			&Mutex{
+				client:        client,
+				key:           keyTwo,
+				expiration:    30 * time.Second,
+				value:         actualTwo.value,
+				retryStrategy: NewAverageRetry(3, 10*time.Second),
+				watchDog:      nil,
+			},
+		},
+	}
+
+	for _, c := range cases {
+		t.Run(c.Name, func(t *testing.T) {
+			compareMutex(t, c.Expected, c.Actual)
+		})
+	}
+}
+
 func compareMutex(t *testing.T, expected *Mutex, actual *Mutex) {
 	t.Helper()
 	if expected.client != actual.client {
@@ -175,6 +254,10 @@ func compareMutex(t *testing.T, expected *Mutex, actual *Mutex) {
 	if fmt.Sprintf("%T", expected.retryStrategy) != fmt.Sprintf("%T", actual.retryStrategy) {
 		t.Errorf("retryStrategy is not equal,expected %v, got %v", expected.retryStrategy, actual.retryStrategy)
 	}
+
+	if expected.watchDog != actual.watchDog {
+		t.Errorf("watchDog is not equal,expected %v, got %v", expected.watchDog, actual.watchDog)
+	}
 }
 
 func compareClient(t *testing.T, expect, actual *Client) {
@@ -188,7 +271,16 @@ func compareClient(t *testing.T, expect, actual *Client) {
 	}
 
 	if expect.Cipher != actual.Cipher {
-		t.Errorf("cipher is not equal,expected %v, got %v", expect.Cipher, actual.Cipher)
+		now := time.Now().String()
+		expectValue := make([]byte, len(now))
+		actualValue := make([]byte, len(now))
+		expect.Cipher.XORKeyStream(expectValue, []byte(now))
+		actual.Cipher.XORKeyStream(actualValue, []byte(now))
+		for i := 0; i < len(actualValue); i++ {
+			if actualValue[i] != expectValue[i] {
+				t.Errorf("cipher is not equal,expected %v, got %v", expect.Cipher, actual.Cipher)
+			}
+		}
 	}
 }
 
